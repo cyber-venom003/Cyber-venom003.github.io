5cc99205fff845132e398c937d74ee92a27886fb
cyber-venom003 <tech.worm.1728@gmail.com>
2020-06-17 12:42:33 +0530

    Slider redefined, video clips added

--- clicknoxinnovations/js/app.js
+++ clicknoxinnovations/js/app.js
@@ -1,5 +1,20 @@
 var slideIndex = 0;
 showSlides();
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
 
 function showSlides() {
   var i;
